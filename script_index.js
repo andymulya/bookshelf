34767d9ccaf86e5db79a0f46670df57bb837ee08
Andy Eka Mulya <andymulya13@gmail.com>
2022-09-17 18:34:29 +0700

    membuat tombol close di form tambah daftar buku

--- script_index.js
+++ script_index.js
@@ -1,18 +1,23 @@
 function formTambahDaftarBuku(){
 	const bTambahDaftarBuku = document.querySelector('div.tombol a:first-child');
+	
+	//Tombol Tambah daftar buku
 	bTambahDaftarBuku.addEventListener('click', function(){
 		const form = 
-		`
+		`	<div class='close'>
+				<div class='kiri'></div>
+				<div class='kanan'></div>
+			</div>
 			<h1>Tambahkan Daftar Buku</h1>
 			<form class='form'>
-		      <label for="judul">Judul:</label>
+		      <label for="judul" class='judul'>Judul:</label>
 		      <input type="text" id="judul">
 
 		      <label for="penerbit">Penerbit:</label>
 		      <input type="text" id="penerbit">
 
 		      <label for="tahun">Tahun:</label>
-		      <input type="number" id="tahun">
+		      <input type="number" id="tahun" >
 
 		      <button>Tambah</button>
 		    </form>
@@ -23,10 +28,14 @@ function formTambahDaftarBuku(){
 
 		createElementSectionForm.appendChild(createElementDivForm);
 		getMain.appendChild(createElementSectionForm);
+
 		createElementSectionForm.setAttribute('class', 'sectionForm');
 		createElementDivForm.setAttribute('class', 'container');
 
 		createElementDivForm.innerHTML = form;
+
+		const getButtonClos = querySelector('.close');
+		getButtonClos.addEventListener('click', () => createElementSectionForm.setAttribute('hidden'))
 	});
 }
 
