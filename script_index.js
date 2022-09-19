12927cf449d84a18ec4fe4cff5934c24b2187e02
Andy Eka Mulya <andymulya13@gmail.com>
2022-09-19 12:11:21 +0700

    menambahkan fungsionalitas pada fitur tambah daftar buku

--- script_index.js
+++ script_index.js
@@ -1,42 +1,59 @@
-function formTambahDaftarBuku(){
-	const bTambahDaftarBuku = document.querySelector('div.tombol a:first-child');
+const getSectionForm = document.querySelector('.sectionForm');
+getTombolTambahDaftarBuku = document.querySelector('.tombol .tambahDaftarBuku'),
+getTombolClose = document.querySelector('.close'),
+getValueJudul = document.getElementById('judul'),
+getValuePenulis = document.getElementById('penulis'),
+getValueTahun = document.getElementById('tahun'),
+getTombolTambah = document.querySelector('.tambah'),
+getValueCheckBox = document.querySelector('#selesai_dibaca'),
+storageKey = 'DATA_BOOK',
+dataBukuArray = [];
+
+//Tombol Tambah daftar buku
+getTombolTambahDaftarBuku.addEventListener('click', function(){
+	getSectionForm.removeAttribute('hidden');
+});
+
+//Tombol close di form
+getTombolClose.addEventListener('click', function(){
+	getSectionForm.setAttribute('hidden','');
+});
+
+//Tombol tambah data buku
+getTombolTambah.addEventListener('click', function(){
+	if(checkStorage()){
+		const getData = {
+			id: +new Date,
+			judul: getValueJudul.value,
+			penulis: getValuePenulis.value,
+			tahun: getValueTahun.value,
+			isComplate: getValueCheckBox.checked
+		}
+		if(getValueJudul.value == '' && getValuePenulis.value == '' && getValueTahun.value == ''){
+			alert('Maaf, mohon diisi dengan benar');
+		}else{
+			dataBukuArray.push(getData);
+
+			const result = JSON.stringify(dataBukuArray);
+			localStorage.setItem(storageKey, result);
+
+			alert('Data buku berhasil ditambahkan');
+
+			getValueJudul.value = '';
+			getValuePenulis.value = '';
+			getValueTahun.value = '';
+			getValueCheckBox.checked = false;
+		}
+	}else{
+		alert('Website anda tidak mendukung storage');
+	}
 	
-	//Tombol Tambah daftar buku
-	bTambahDaftarBuku.addEventListener('click', function(){
-		const form = 
-		`	<div class='close'>
-				<div class='kiri'></div>
-				<div class='kanan'></div>
-			</div>
-			<h1>Tambahkan Daftar Buku</h1>
-			<form class='form'>
-		      <label for="judul" class='judul'>Judul:</label>
-		      <input type="text" id="judul">
+});
 
-		      <label for="penerbit">Penerbit:</label>
-		      <input type="text" id="penerbit">
+//Cek dukungan localStorage pada web
+const checkStorage = () => typeof(Storage) !== 'undifine';
 
-		      <label for="tahun">Tahun:</label>
-		      <input type="number" id="tahun" >
 
-		      <button>Tambah</button>
-		    </form>
-		`;
-		const createElementSectionForm = document.createElement('section');
-		const createElementDivForm = document.createElement('div.form');
-		const getMain = document.querySelector('main');
 
-		createElementSectionForm.appendChild(createElementDivForm);
-		getMain.appendChild(createElementSectionForm);
 
-		createElementSectionForm.setAttribute('class', 'sectionForm');
-		createElementDivForm.setAttribute('class', 'container');
 
-		createElementDivForm.innerHTML = form;
-
-		const getButtonClos = querySelector('.close');
-		getButtonClos.addEventListener('click', () => createElementSectionForm.setAttribute('hidden'))
-	});
-}
-
-formTambahDaftarBuku();
