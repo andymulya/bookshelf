c649ecc88534139d53c07a8e56df846de2a20729
Andy Eka Mulya <andymulya13@gmail.com>
2022-09-20 11:52:08 +0700

    menambahkan fungsional ketika menyimpan data

--- script_index.js
+++ script_index.js
@@ -1,13 +1,19 @@
-const getSectionForm = document.querySelector('.sectionForm');
+const getSectionForm = document.querySelector('.sectionForm'),
+getSectionDaftarBuku = document.querySelector('.sectionDaftarBuku');
 getTombolTambahDaftarBuku = document.querySelector('.tombol .tambahDaftarBuku'),
-getTombolClose = document.querySelector('.close'),
+getTombolCloseForm = document.querySelector('.closeForm'),
+getTombolLihatDaftarBuku = document.querySelector('.lihatDaftarBuku'),
+getTombolCloseDaftarBuku = document.querySelector('.closeDaftarBuku');
 getValueJudul = document.getElementById('judul'),
 getValuePenulis = document.getElementById('penulis'),
 getValueTahun = document.getElementById('tahun'),
 getTombolTambah = document.querySelector('.tambah'),
 getValueCheckBox = document.querySelector('#selesai_dibaca'),
-storageKey = 'DATA_BOOK',
+storageKey = 'DATA_BUKU',
 dataBukuArray = [];
+
+//Cek dukungan localStorage pada web
+const checkStorage = () => typeof(Storage) !== 'undifined';
 
 //Tombol Tambah daftar buku
 getTombolTambahDaftarBuku.addEventListener('click', function(){
@@ -15,14 +21,26 @@ getTombolTambahDaftarBuku.addEventListener('click', function(){
 });
 
 //Tombol close di form
-getTombolClose.addEventListener('click', function(){
+getTombolCloseForm.addEventListener('click', function(){
 	getSectionForm.setAttribute('hidden','');
+});
+
+//Tombol Lihat daftar buku
+getTombolLihatDaftarBuku.addEventListener('click', function(){
+	getSectionDaftarBuku.removeAttribute('hidden');
+	dataBukuArray = JSON.parse(localStorage.getItem(storageKey));
+
+	console.log(dataBukuArray);
+});
+
+//Tombol close di lihat daftar buku
+getTombolCloseDaftarBuku.addEventListener('click', function(){
+	getSectionDaftarBuku.setAttribute('hidden','');
 });
 
 //Tombol tambah data buku
 getTombolTambah.addEventListener('click', function(){
-	if(checkStorage()){
-		const getData = {
+	const getData = {
 			id: +new Date,
 			judul: getValueJudul.value,
 			penulis: getValuePenulis.value,
@@ -33,6 +51,7 @@ getTombolTambah.addEventListener('click', function(){
 			alert('Maaf, mohon diisi dengan benar');
 		}else{
 			dataBukuArray.push(getData);
+			
 
 			const result = JSON.stringify(dataBukuArray);
 			localStorage.setItem(storageKey, result);
@@ -44,16 +63,21 @@ getTombolTambah.addEventListener('click', function(){
 			getValueTahun.value = '';
 			getValueCheckBox.checked = false;
 		}
-	}else{
-		alert('Website anda tidak mendukung storage');
-	}
-	
 });
 
-//Cek dukungan localStorage pada web
-const checkStorage = () => typeof(Storage) !== 'undifine';
-
-
-
+window.addEventListener('load', function(){
+	if(checkStorage()){
+		
+		if(localStorage.key(storageKey)){
+			const getDataLocal = JSON.parse(localStorage.getItem(storageKey));
+			for(data of getDataLocal){
+		 		dataBukuArray.push(data);
+		 		console.log(dataBukuArray);
+		 	}
+		}
+	}else{
+		alert('Maaf web browser anda tidak mendukung');
+	}
+});
 
 
